013380f36b6f1a45db8dd0260314734645448b3d
laritzamerida <laritzamerida@gmail.com>
2023-08-18 09:08:06 -0300

    ultima version

--- js/products.js
+++ js/products.js
@@ -1,89 +1,61 @@
 const products = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
-//función para mostrar el spinner de carga:
-function showSpinner(){
-    document.getElementById("spinner-wrapper").style.display = "block"; 
-  }
-  
-  //función para ocultar el spinner de carga:
-  function hideSpinner(){
-    document.getElementById("spinner-wrapper").style.display = "none";
-  }
 
-async function getJSONData2(url){
-    let result = {};
-    showSpinner(); 
-    return fetch(url) 
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner(); 
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner(); 
-        return result;
-    });
-}
-
-//array donde se cargarán los datos recibidos:
-let categoriesArray = [];
-
-//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
-function showCategoriesList(array){
-    let htmlContentToAppend = "";
-
-    for(let i = 0; i < array.length; i++){ 
-        let category = array[i];
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.productCount +`</h4> 
-                        <p> `+ category.description +`</p> 
-                        </div>
-                        <small class="text-muted">` + category.name + ` artículos</small> 
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        `
-        
-    }
-    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+async function getJSONData2(url) {
+    return fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function(response) {
+            return { status: 'ok', data: response };
+        })
+        .catch(function(error) {
+            return { status: 'error', data: error };
+        });
 }
 
 
-/* 
-EJECUCIÓN:
-
--Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
--Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
--Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
-
-*/
-
-document.addEventListener("DOMContentLoaded", function(){
-    getJSONData2(products).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
+document.addEventListener("DOMContentLoaded", function() {
+    getJSONData2(products).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            const categoriesArray = resultObj.data.products;
             showCategoriesList(categoriesArray);
         }
     });
 });
+
+
+function showCategoriesList(array) {
+    let htmlContentToAppend = "";
+
+
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.image}" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                            <h4>${product.name}-${product.currency} ${product.cost} </h4>
+                            <p>${product.description}</p>
+                        </div>
+                        <small class="text-muted"> ${product.soldCount} vendidos </small>
+                    </div>
+                    <small class="text-muted"> </small>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+}
+
+
