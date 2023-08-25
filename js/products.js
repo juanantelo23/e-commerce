95a7046c005af1d9148b8daa9ec575287dc3b6ce
Nicolás <106615305+Forneiro@users.noreply.github.com>
2023-08-25 13:22:47 -0300

    agregar filtros

--- js/products.js
+++ js/products.js
@@ -59,6 +59,129 @@ function showCategoriesList(array) {
     document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
 }
 
+// Ordena de forma descendente          
+let ascendente = document.getElementById("sortByCountDow");
+ascendente.addEventListener("click", function () {
+    const catID = localStorage.getItem("catID");
+    const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let productosOrdenados = ordenarProductosAscendente(data.products);
+            showCategoriesList(productosOrdenados);
+            let productosHTML = "";
+            for (let i = 0; i < productosOrdenados.length; i++) {
+                productosHTML +=
+                    `
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${data[i].image}" alt="product image" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                    <h4>${data[i].name}-${data[i].currency} ${data[i].cost} </h4>
+                                    <p>${data[i].description}</p>
+                                </div>
+                                <small class="text-muted"> ${data[i].soldCount} vendidos </small>
+                            </div>
+                            <small class="text-muted"> </small>
+                        </div>
+                    </div>
+                </div>
+                `
+            };
+            document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        })
+
+});
+function ordenarProductosAscendente(productos) {
+    return productos.slice().sort((a, b) => parseFloat(a.cost) - parseFloat(b.cost));
+}
+
+// Ordena de forma descendente
+let descendente = document.getElementById("sortByCountUp");
+descendente.addEventListener("click", function () {
+    const catID = localStorage.getItem("catID");
+    const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let productosOrdenados = ordenarProductosDescendente(data.products);
+            showCategoriesList(productosOrdenados);
+            let productosHTML = "";
+            for (let i = 0; i < productosOrdenados.length; i++) {
+                productosHTML +=
+                    `
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${data[i].image}" alt="product image" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                    <h4>${data[i].name}-${data[i].currency} ${data[i].cost} </h4>
+                                    <p>${data[i].description}</p>
+                                </div>
+                                <small class="text-muted"> ${data[i].soldCount} vendidos </small>
+                            </div>
+                            <small class="text-muted"> </small>
+                        </div>
+                    </div>
+                </div>
+                `
+            };
+            document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        })
+
+});
+function ordenarProductosDescendente(productos) {
+    return productos.slice().sort((b, a) => parseFloat(a.cost) - parseFloat(b.cost));
+}
+
+// Relevancia
+let relevancia = document.getElementById("sortByRel");
+relevancia.addEventListener("click", function () {
+    const catID = localStorage.getItem("catID");
+    const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let productosOrdenados = ordenarProductosRelevancia(data.products);
+            showCategoriesList(productosOrdenados);
+            let productosHTML = "";
+            for (let i = 0; i < productosOrdenados.length; i++) {
+                productosHTML +=
+                    `
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${data[i].image}" alt="product image" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                    <h4>${data[i].name}-${data[i].currency} ${data[i].cost} </h4>
+                                    <p>${data[i].description}</p>
+                                </div>
+                                <small class="text-muted"> ${data[i].soldCount} vendidos </small>
+                            </div>
+                            <small class="text-muted"> </small>
+                        </div>
+                    </div>
+                </div>
+                `
+            };
+            document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        })
+});
+
+function ordenarProductosRelevancia(productos) {
+    return productos.sort((b, a) => parseInt(a.soldCount) - parseInt(b.soldCount));
+}
+
 function updateCategoryTitle(categoryName, categoryTitle) {
     categoryTitle.innerHTML = categoryName;
   }
