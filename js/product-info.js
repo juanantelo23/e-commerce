8e3c39ff9df1030281826476ea9a8edeab656f0a
laritzamerida <laritzamerida@gmail.com>
2023-09-10 18:43:32 -0300

    Pauats 1 y 2 terminadas

--- js/product-info.js
+++ js/product-info.js
@@ -1,23 +1,23 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const dato = localStorage.getItem("productID");
-    console.log(dato)
-  })
-  
-  
-  document.addEventListener("DOMContentLoaded", function () {
+
+
     // Obtener el ID del producto almacenado en el almacenamiento local
     const productID = localStorage.getItem("productID");
   
   
-    // Construir la URL de la API usando el productID
+    // Construir la URL del JSON
     const catID = localStorage.getItem("catID");
-    const apiUrl = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+    const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
   
   
-    // Realizar una solicitud GET a la API
-    fetch(apiUrl)
+    // Obtener el elemento donde se mostrarán los detalles del producto
+    const productInfoContainer = document.getElementById('product-info-container');
+  
+  
+    // Realizar una solicitud GET al JSON
+    fetch(url)
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
@@ -25,44 +25,31 @@ document.addEventListener('DOMContentLoaded', function () {
         return response.json();
       })
       .then(data => {
-        const dataArray = Object.values(data);
+        // Buscar el producto seleccionado por su ID
+        const selectedProduct = data.products.find(product => product.id === parseInt(productID));
   
   
-       
-        JSON.stringify(dataArray);
-        console.log(dataArray);
-        // Verificar si "dataArray" es una matriz
-        if (Array.isArray(dataArray)) {
-          // Buscar el producto seleccionado por su ID
-          const selectedProduct = dataArray.find(product => product.id === productID);
-  
-  
-          if (selectedProduct) {
-            // Mostrar los detalles del producto en la página
-            const productDetailsContainer = document.getElementById("product-info-container");
-            productDetailsContainer.innerHTML = `
-                        <h2>${selectedProduct.name}</h2>
-                        <p>${selectedProduct.description}</p>
-                        <p>Precio: ${selectedProduct.currency} ${selectedProduct.cost}</p>
-                        <p>Cantidad de vendidos: ${selectedProduct.soldCount}</p>
-                        <p>Categoría: ${selectedProduct.category}</p>
+        if (selectedProduct) {
+          // Llenar el contenedor con los detalles del producto
+          productInfoContainer.innerHTML = `
+                    <div class="container product-info">
+                        <h2 class="p-info" id="product-name">${selectedProduct.name}</h2>
+                        <p>Precio <span id="product-price">${selectedProduct.currency} ${selectedProduct.cost}</span></p>
+                        <p>Descripción <span id="product-description">${selectedProduct.description}</span></p>
+                        <p>Categoría <span id="product-category">${data.catName}</span></p>
+                        <p>Cantidad de vendidos <span id="product-soldCount">${selectedProduct.soldCount}</span></p>
+                        <p>Imágenes ilustrativas</p>
                         <img src="${selectedProduct.image}" alt="${selectedProduct.name}">
-                        <!-- Otros detalles del producto aquí -->
-                    `;
-          } else {
-            console.error("Producto no encontrado");
-          }
+                    </div>
+                `;
         } else {
-          console.error("Los datos de la API no son una matriz");
+          console.error("Producto no encontrado");
         }
       })
       .catch(error => {
         console.error("Error al cargar los detalles del producto:", error);
       });
   });
-  
-  
-  
   
   
   
