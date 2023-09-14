9cf2db6a088fef552295ecf13b40683da565273b
kt-soo <137802962+kt-soo@users.noreply.github.com>
2023-09-14 00:07:32 -0300

    Entrega 3 (Terminado)
    Agregamos las estrellas a los comentarios según corresponde al score que viene del JSON.
    Agregamos la funcionalidad de calificar el nuevo comentario y que éste se suba a la lista de comentarios existentes.
    
    Co-Authored-By: anthonysacevedo <141374766+anthonysacevedo@users.noreply.github.com>
    Co-Authored-By: Maru <141073022+marucaballero@users.noreply.github.com>

--- js/product-info.js
+++ js/product-info.js
@@ -27,18 +27,18 @@ document.addEventListener('DOMContentLoaded', function () {
       if (selectedProduct) {
         // Llenar el contenedor con los detalles del producto
         productInfoContainer.innerHTML = `
-                  <div class="container product-info">
-                      <h2 class="p-info" id="product-name">${selectedProduct.name}</h2>
-                      <p>Precio <span id="product-price">${selectedProduct.currency} ${selectedProduct.cost}</span></p>
-                      <p>Descripción <span id="product-description">${selectedProduct.description}</span></p>
-                      <p>Categoría <span id="product-category">${data.catName}</span></p>
-                      <p>Cantidad de vendidos <span id="product-soldCount">${selectedProduct.soldCount}</span></p>
-                      <p>Imágenes ilustrativas</p>
-                      <img src="${selectedProduct.image}" alt="${selectedProduct.name}">
-                  </div>
-              `;
+          <div class="container product-info">
+            <h2 class="p-info" id="product-name">${selectedProduct.name}</h2>
+            <p>Precio <span id="product-price">${selectedProduct.currency} ${selectedProduct.cost}</span></p>
+            <p>Descripción <span id="product-description">${selectedProduct.description}</span></p>
+            <p>Categoría <span id="product-category">${data.catName}</span></p>
+            <p>Cantidad de vendidos <span id="product-soldCount">${selectedProduct.soldCount}</span></p>
+            <p>Imágenes ilustrativas</p>
+            <img src="${selectedProduct.image}" alt="${selectedProduct.name}">
+          </div>
+        `;
+        // Construccion de la box de comentarios 
 
-        // Box de comentarios
         // Construir la URL de comentarios utilizando el ID del producto
         const commentsUrl = `https://japceibal.github.io/emercado-api/products_comments/${productID}.json`;
         // Realizar una solicitud GET a la URL de comentarios
@@ -57,11 +57,10 @@ document.addEventListener('DOMContentLoaded', function () {
               const commentElement = document.createElement('div');
               commentElement.classList.add('comment');
               commentElement.innerHTML = `
-            
                 <h3>${comment.user}:</h3>
                 <p>${comment.description}</p>
                 <p>${comment.dateTime}</p>
-                
+                <div class="comment-stars">${generateStars(comment.score)}</div>
               `;
               commentsContainer.appendChild(commentElement);
             });
@@ -86,13 +85,15 @@ commentForm.addEventListener('submit', function (e) {
   // Recopila los valores del formulario
   const name = document.getElementById('name').value;
   const commentText = document.getElementById('comment').value;
+  const score = getSelectedStarCount(); // Obtiene la cantidad de estrellas seleccionadas
 
-  // Crea un nuevo comentario y agrega la fecha aleatoria
+  // Crea un nuevo comentario y agrega la fecha actual
   const newComment = `
     <div class="comment">
       <h3>${name}:</h3>
       <p>${commentText}</p>
       <p>${new Date().toLocaleString()}</p>
+      <div class="comment-stars">${generateStars(score)}</div>
     </div>
   `;
 
@@ -102,4 +103,43 @@ commentForm.addEventListener('submit', function (e) {
   // Limpia el formulario
   document.getElementById('name').value = '';
   document.getElementById('comment').value = '';
+
+  // Limpia las estrellas seleccionadas
+  clearSelectedStars();
 });
+
+// Estrellas para el formulario
+const stars = document.querySelectorAll(".divStar span");
+// Pinta todas las estrellas anteriores a la seleccionada
+stars.forEach((star, index1) => {
+  star.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+      index1 >= index2 ? star.classList.add("checked") : star.classList.remove("checked");
+    });
+  });
+});
+
+// Función para obtener la cantidad de estrellas seleccionadas
+function getSelectedStarCount() {
+  const selectedStars = document.querySelectorAll(".divStar span.checked");
+  return selectedStars.length;
+}
+
+// Función para limpiar las estrellas seleccionadas
+function clearSelectedStars() {
+  const selectedStars = document.querySelectorAll(".divStar span.checked");
+  selectedStars.forEach(star => star.classList.remove("checked"));
+}
+
+// Función para generar las estrellas en los comentarios
+function generateStars(score) {
+  let starsHtml = '';
+  for (let i = 0; i < 5; i++) {
+    if (i < score) {
+      starsHtml += '<span class="fa fa-star checked"></span>';
+    } else {
+      starsHtml += '<span class="fa fa-star"></span>';
+    }
+  }
+  return starsHtml;
+}
