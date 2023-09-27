453fec677aa583f5a850789c1c01f4749f938244
kt-soo <137802962+kt-soo@users.noreply.github.com>
2023-09-26 23:36:05 -0300

    Pauta 2 (finalizada)
    Se agregó la barra de opciones al nombre de usuario desde el init.js, además movimos la etiqueta a del html y le colocamos estilo.
    Más adelante podremos poner iconos y personalizar mejor.

--- js/init.js
+++ js/init.js
@@ -39,16 +39,32 @@ let getJSONData = function(url){
         return result;
     });
 }
-
 //Método para imprimir el nombre de usuario en la barra de navegación
 document.addEventListener("DOMContentLoaded", function(){
   var userName = localStorage.getItem("user_name"); //toma los datos guardados en login
   var insertName = document.createElement("a"); //crea una etiqueta "a" en la barra de navegación
   insertName.classList.add('nav-link'); //Inserta la clase existente para las etiquetas "a" del top-nav
 
+
   if (userName) {
-      insertName.innerHTML = userName; //toma el dato de la variable userName y lo imprime en la top-nav
+      insertName.innerHTML = `    
+  <div class="dropdown">
+  <a class="navLink dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ${userName}
+          </a>
+  <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+    <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
+    <li><a class="dropdown-item" href="my-profile.html">Mi Perfil</a></li>
+    <li><a class="dropdown-item" href="login.html" id="cerrarSesion">Cerrar Sesion</a></li>
+  </ul>
+</div>
+      `; //toma el dato de la variable userName y lo imprime en la top-nav
       document.getElementById('user').appendChild(insertName); //agrega la etiqueta creada como hijo del elemento li
   }
+
+
+  document.getElementById("cerrarSesion").addEventListener("click", function(event) {
+    localStorage.removeItem("user_name"); //Al dar click en "Cerrar Sesión" elimina el dato de usuario almacenado en el local storage
+  })
 });
-//
+
