6f12f2086b5403d8a5da3aba0c6fb7ead835fbe6
laritzamerida <laritzamerida@gmail.com>
2023-10-20 15:22:37 -0300

    Quedó el cálculo del subtotal general

--- js/cart.js
+++ js/cart.js
@@ -2,63 +2,84 @@ document.addEventListener('DOMContentLoaded', function () {
     const compras = localStorage.getItem("listaCompra");
     const listaCompra = JSON.parse(compras);
     const cartContainer = document.getElementById('cartContainer');
-
+    const subtotalGeneralDiv = document.getElementById('Subtotal-General');
+   
     // Verificar si el contenedor del carrito existe
     if (!cartContainer) {
         console.error('No se encontró el contenedor del carrito.');
         return;
     }
 
+    // Función para calcular el subtotal de un producto
+    function calcularSubtotal(item, cantidad) {
+        const precio = item.cost;
+        return cantidad * precio;
+    }
+
+    
+    let subtotalGeneral = 0;
+    let costoEnvio = 0;
+   
+
     for (let i = 0; i < listaCompra.length; i++) {
         let item = listaCompra[i];
         const htmlContentToAppend = `
-            <div class="row product-row">
-                <div class="col-md-2">
-                    <img src="${item.images[1]}" alt="${item.name}" style="width: 100px;">
-                </div>
-                <div class="col-md-2">
-                    <div class="sub-title fw-bold">Nombre:</div>
-                    <div>${item.name}</div>
-                </div>
-                <div class="col-md-2">
-                    <div class="sub-title fw-bold">Costo:</div>
-                    <div>${item.cost} ${item.currency}</div>
-                </div>
-                <div class="col-md-2">
-                    <div class="sub-title fw-bold ">Cantidad:</div>
-                    <div class="col-6"><input type="number" class="form-control" value="1" id="cantidad-${item.id}" min="1" max="99">
-                    </div>
-                </div>
-                <div class="col-md-2 fw-bold">
-                    <div class="sub-title">Sub total:</div>
-                    <div class="font-weight-bold" id="subtotal-${item.id}"> ${item.currency} ${item.cost} </div>
+        <div class="row product-row">
+            <div class="col-md-2">
+                <img src="${item.images[1]}" alt="${item.name}" style="width: 100px;">
+            </div>
+            <div class="col-md-2">
+                <div class="sub-title fw-bold">Nombre:</div>
+                <div>${item.name}</div>
+            </div>
+            <div class="col-md-2">
+                <div class="sub-title fw-bold">Costo:</div>
+                <div>${item.cost} ${item.currency}</div>
+            </div>
+            <div class="col-md-2">
+                <div class="sub-title fw-bold ">Cantidad:</div>
+                <div class="col-6"><input type="number" class="form-control" value="1" id="cantidad-${item.id}" min="1" max="99">
                 </div>
             </div>
-            <hr class="divider">
-        `;
+            <div class="col-md-2 fw-bold">
+                <div class="sub-title">Sub total:</div>
+                <div class="font-weight-bold" id="subtotal-${item.id}"> ${item.currency} ${item.cost} </div>
+            </div>
+        </div>
+        <hr class="divider">
+    `;
 
-        // Agregar el contenido HTML al final del contenedor
         cartContainer.insertAdjacentHTML('beforeend', htmlContentToAppend);
 
-        // Obtener el elemento de cantidad y subtotal
         const cantidadInput = document.getElementById(`cantidad-${item.id}`);
         const subtotalDiv = document.getElementById(`subtotal-${item.id}`);
-
-        // Verificar si los elementos se han encontrado correctamente
+        
         if (cantidadInput && subtotalDiv) {
-            // Agregar un evento 'input' al elemento de cantidad
             cantidadInput.addEventListener("input", function () {
-                const cantidad = parseInt(cantidadInput.value, 10) || 0; // Obtener la cantidad como un número entero
-                const precio = item.cost;
-                const subtotal = cantidad * precio; // Calcular el subtotal
-
-                // Mostrar el subtotal con el formato correcto
+                const cantidad = parseInt(cantidadInput.value, 10) || 0;
+                const subtotal = calcularSubtotal(item, cantidad);
                 subtotalDiv.textContent = `${item.currency} ${subtotal}`;
+
+                subtotalGeneral = listaCompra.reduce((total, product) => {
+                    const productCantidad = parseInt(document.getElementById(`cantidad-${product.id}`).value, 10) || 0;
+                    return total + calcularSubtotal(product, productCantidad);
+                }, 0);
+
+                subtotalGeneralDiv.textContent = `${item.currency} ${subtotalGeneral}`;
+
             });
         } else {
             console.error('No se encontraron los elementos de cantidad y subtotal.');
         }
+
+        subtotalGeneral += calcularSubtotal(item, 1); // Suponiendo que la cantidad inicial es 1
+        
+
     }
+
+    subtotalGeneralDiv.textContent = `${listaCompra[0].currency} ${subtotalGeneral}`;
+});
+
 
     // Forma de Pago
     const exampleModal = document.getElementById('exampleModal')
@@ -114,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-});
+
 
 // Validación de form y cambio estilo 
 (function () {
